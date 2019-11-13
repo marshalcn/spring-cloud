15ea9235784fc2e47f79705ec523cac681242276
Max <damascus10xiaoma@163.com>
2019-11-13 20:28:28 +0800

    push config client

---
+++ springcloud-config-client-3355/src/main/java/com/mx/StartSpringCLoudApplication.java
@@ -0,0 +1,11 @@
+package com.mx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartSpringCLoudApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StartSpringCLoudApplication.class, args) ;
+    }
+}
